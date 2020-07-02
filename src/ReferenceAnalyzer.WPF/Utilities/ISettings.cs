06be8c4bc74cd85bead7ce633d3c2c9e346cb2a7
Matmalecki <mmateusz314@gmail.com>
2020-07-02 15:41:41 +0200

    Added initial settings for ui

---
+++ src/ReferenceAnalyzer.WPF/Utilities/ISettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReferenceAnalyzer.WPF.Utilities
+{
+    public interface ISettings
+    {
+        string SolutionPath { get; set; }
+    }
+}
---
+++ src/ReferenceAnalyzer.WPF/Utilities/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+
+namespace ReferenceAnalyzer.WPF.Utilities
+{
+    public class Settings : ISettings
+    {
+        private readonly Configuration _Settings;
+        private const string _SolutionPathKey = "SolutionPath";
+        public string SolutionPath
+        {
+            get => _Settings.AppSettings.Settings[_SolutionPathKey]?.Value;
+            set
+            {
+                var settings = _Settings.AppSettings.Settings;
+                if (settings[_SolutionPathKey] == null)
+                {
+                    settings.Add(_SolutionPathKey, value);
+                }
+                else
+                {
+                    settings[_SolutionPathKey].Value = value;
+                }
+                _Settings.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+        public Settings()
+        {
+            _Settings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        }
+    }
+
+}
