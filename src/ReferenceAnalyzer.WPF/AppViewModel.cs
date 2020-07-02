399c6ecf261059f6f5f036ecfd8da27fc9945e9b
Matmalecki <mmateusz314@gmail.com>
2020-07-02 15:41:41 +0200

    Added initial settings for ui

---
+++ src/ReferenceAnalyzer.WPF/AppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReferenceAnalyzer.WPF.Utilities;
+
+namespace ReferenceAnalyzer.WPF
+{
+    public class AppViewModel : ReactiveObject
+    {
+        private string _Path;
+
+        public AppViewModel(ISettings settings)
+        {
+            _Path = settings.SolutionPath;
+            var canLoad = this.WhenAnyValue(x => x.Path, path => !string.IsNullOrEmpty(path));
+
+            this.WhenAnyValue(viewModel => viewModel.Path).Subscribe(x => settings.SolutionPath = x);
+        }
+
+        public string Path
+        {
+            get => _Path;
+            set => this.RaiseAndSetIfChanged(ref _Path, value);
+        }
+
+    }
+}
--- src/ReferenceAnalyzer.WPF/MainWindow.xaml.cs
+++ src/ReferenceAnalyzer.WPF/MainWindow.xaml.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using ReactiveUI;
+using ReferenceAnalyzer.WPF.Utilities;
+using System.Reactive.Disposables;
 
 namespace ReferenceAnalyzer.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new AppViewModel(new Settings());
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.Bind(ViewModel,
+                    viewModel => viewModel.Path,
+                    view => view.Path.Text
+                    ).DisposeWith(disposableRegistration);
+            });
         }
     }
 }
