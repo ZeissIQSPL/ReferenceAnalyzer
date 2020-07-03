179126179f3606154c9b12aa4444c661ac0e6d9e
Matmalecki <mmateusz314@gmail.com>
2020-07-03 08:59:39 +0200

    Added first xaml controls

--- src/ReferenceAnalyzer.WPF/AppViewModel.cs
+++ src/ReferenceAnalyzer.WPF/AppViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Reactive;
 using System.Reactive.Linq;
 using ReactiveUI;
+using ReferenceAnalyzer.Core;
 using ReferenceAnalyzer.WPF.Utilities;
 
 namespace ReferenceAnalyzer.WPF
@@ -9,10 +11,16 @@ namespace ReferenceAnalyzer.WPF
     {
         private string _Path;
 
-        public AppViewModel(ISettings settings)
+        public AppViewModel(ISettings settings, IProjectProvider projectProvider)
         {
             _Path = settings.SolutionPath;
             var canLoad = this.WhenAnyValue(x => x.Path, path => !string.IsNullOrEmpty(path));
+
+            Load = ReactiveCommand.Create(() =>
+            {
+                return projectProvider.GetReferences(Path);
+            }, canLoad);
+
 
             this.WhenAnyValue(viewModel => viewModel.Path).Subscribe(x => settings.SolutionPath = x);
         }
@@ -22,6 +30,9 @@ namespace ReferenceAnalyzer.WPF
             get => _Path;
             set => this.RaiseAndSetIfChanged(ref _Path, value);
         }
+
+        public ReactiveCommand<Unit, ReferencesReport> Load { get; }
+
 
     }
 }
---
+++ src/ReferenceAnalyzer.WPF/Utilities/FakeProjectProvider.cs
@@ -0,0 +1,17 @@
+﻿using ReferenceAnalyzer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReferenceAnalyzer.WPF.Utilities
+{
+    class FakeProjectProvider : IProjectProvider
+    {
+        public ReferencesReport GetReferences(string path)
+        {
+            return new ReferencesReport(new[] { "ref1", "ref2", "ref3" }, 
+                new[] { new ActualReference("ref1", Enumerable.Empty<ReferenceOccurrence>()) });
+        }
+    }
+}
