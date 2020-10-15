a60cba2e0fec6a2e211054075d7d536fb75a8f70
Matmalecki <mmateusz314@gmail.com>
2020-10-15 11:27:03 +0200

    Prepared settings for Last loaded solutions list

--- src/ReferenceAnalyzer.UI/Models/ISettings.cs
+++ src/ReferenceAnalyzer.UI/Models/ISettings.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
 namespace ReferenceAnalyzer.UI.Models
 {
     public interface ISettings
     {
         string SolutionPath { get; set; }
+
+        IImmutableList<string> LastLoadedSolutions { get; set; }
     }
 }
--- src/ReferenceAnalyzer.UI/Models/Settings.cs
+++ src/ReferenceAnalyzer.UI/Models/Settings.cs
@@ -1,11 +1,17 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Configuration;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.VisualBasic;
 
 namespace ReferenceAnalyzer.UI.Models
 {
     public class Settings : ISettings
     {
         private const string SolutionPathKey = "SolutionPath";
-        private readonly Configuration _settings;
+        private const string LastSolutionsKey = "LastSolutions";
+        private readonly System.Configuration.Configuration _settings;
 
         public Settings() => _settings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
@@ -16,11 +22,34 @@ namespace ReferenceAnalyzer.UI.Models
             {
                 var settings = _settings.AppSettings.Settings;
                 if (settings[SolutionPathKey] == null)
+                {
                     settings.Add(SolutionPathKey, value);
+                }
                 else
+                {
                     settings[SolutionPathKey].Value = value;
+                    if (!LastLoadedSolutions.Contains(value))
+                    {
+                        LastLoadedSolutions = LastLoadedSolutions.Add(value);
+                    }
+                }
                 _settings.Save(ConfigurationSaveMode.Modified);
             }
         }
+
+        public IImmutableList<string> LastLoadedSolutions {
+            get {
+                var serializedJson = _settings.AppSettings.Settings[SolutionPathKey]?.Value;
+                return serializedJson == null ? JsonSerializer.Deserialize<IImmutableList<string>>(serializedJson) : ImmutableList.Create<string>();
+            }
+            set  {
+                var settings = _settings.AppSettings.Settings;
+                if (settings[LastSolutionsKey] == null)
+                    settings.Add(LastSolutionsKey, JsonSerializer.Serialize(value));
+                else
+                    settings[LastSolutionsKey].Value = JsonSerializer.Serialize(value);
+                _settings.Save(ConfigurationSaveMode.Modified);
+                }
+            }
     }
 }
