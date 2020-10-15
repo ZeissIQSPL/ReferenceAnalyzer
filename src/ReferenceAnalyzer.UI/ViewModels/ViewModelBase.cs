f808bab60f3e7fb02f3ce63badd9df34f23b6801
Matmalecki <mmateusz314@gmail.com>
2020-10-15 12:22:03 +0200

    Fixed after rebase

---
+++ src/ReferenceAnalyzer.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace ReferenceAnalyzer.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
