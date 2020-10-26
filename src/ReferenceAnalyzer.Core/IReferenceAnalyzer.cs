f0b0c51855cccfea3f6b89f9d7aaa2f63058bd6d
Artur-Laskowski <laskowski.artur.96@gmail.com>
2020-10-26 09:28:32 +0100

    Removed overloads without cancellation support in favor of setting default value of token parameters which evaluate to CancellationToken.None

--- src/ReferenceAnalyzer.Core/IReferenceAnalyzer.cs
+++ src/ReferenceAnalyzer.Core/IReferenceAnalyzer.cs
@@ -10,16 +10,11 @@ namespace ReferenceAnalyzer.Core
         IDictionary<string, string> BuildProperties { get; set; }
         bool ThrowOnCompilationFailures { get; set; }
         IProgress<double> ProgressReporter { get; set; }
-        IAsyncEnumerable<ReferencesReport> AnalyzeAll(string solutionPath);
-        Task<ReferencesReport> Analyze(string target);
-        Task<IEnumerable<string>> Load(string solution);
-        IAsyncEnumerable<ReferencesReport> AnalyzeAll();
-        IAsyncEnumerable<ReferencesReport> Analyze(IEnumerable<string> projects);
 
-        IAsyncEnumerable<ReferencesReport> AnalyzeAll(string solutionPath, CancellationToken token);
-        Task<ReferencesReport> Analyze(string target, CancellationToken token);
-        Task<IEnumerable<string>> Load(string solution, CancellationToken token);
-        IAsyncEnumerable<ReferencesReport> AnalyzeAll(CancellationToken token);
-        IAsyncEnumerable<ReferencesReport> Analyze(IEnumerable<string> projects, CancellationToken token);
+        IAsyncEnumerable<ReferencesReport> AnalyzeAll(string solutionPath, CancellationToken token = default);
+        Task<ReferencesReport> Analyze(string target, CancellationToken token = default);
+        Task<IEnumerable<string>> Load(string solution, CancellationToken token = default);
+        IAsyncEnumerable<ReferencesReport> AnalyzeAll(CancellationToken token = default);
+        IAsyncEnumerable<ReferencesReport> Analyze(IEnumerable<string> projects, CancellationToken token = default);
     }
 }
